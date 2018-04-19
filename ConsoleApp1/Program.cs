ead3ba0888b31f47bdf5661bef5505390ecea9ed
michael stewart <mstewart1972@yahoo.com>
2018-04-19 18:58:03 -0400

    initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Program();
+            var cl = new ClassLibrary1.Class1();
+            int a = 2;
+            int b = 2;
+            int c =cl.Method1(a, b);
+            Console.WriteLine(c);
+        }
+
+    }
+}
